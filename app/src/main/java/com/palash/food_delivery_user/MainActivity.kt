1200778146f2b2a78563d6c1e8827838c71809e1
Palash <palashnayek111@gmail.com>
2021-07-09 09:30:21 +0530

    create project

---
+++ app/src/main/java/com/palash/food_delivery_user/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.palash.food_delivery_user
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
